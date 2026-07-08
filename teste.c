4d6d370671fe2640e897e8bf0070604f1598ea22
Dourado <gabrields160326@gmail.com>
2026-07-08 15:22:41 -0300

    alteração

--- teste.c
+++ teste.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int i;
+int i;
+int valor[] = {10, 20, 32, 421, 21};
+int valor2[] = {5, 15, 25, 35, 45};
 
-    for(i=0; i<10; i++){
-        printf("%d", i);
+    printf("\n");
+
+    for(i=0; i<5; i++){
+        printf("%d\n", valor[i]);
     }
+
+    printf("\n");
+
+    for(i = 0; i<5; i++){
+        printf("%d\n", valor2[i]);
+    }
+
+    printf("\n");
+
     return 0;
 }
